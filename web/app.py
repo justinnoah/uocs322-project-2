2cb496f7828ab33532aabb964fc34896752343d8
Justin Noah <justinnoah@gmail.com>
2023-10-12 14:28:11 -0700

    Use config.py from project-1
    Make config available to the app

--- web/app.py
+++ web/app.py
@@ -3,8 +3,11 @@ John Doe's Flask API.
 """
 
 from flask import Flask
+import config
 
 app = Flask(__name__)
+def get_options():
+    return config.configuration()
 
 @app.route("/")
 def hello():
@@ -12,3 +15,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
+    opts = get_options()
